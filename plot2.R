543bff98530285764fba56944bb06163bdca7e42
brightenlx <brightenlx@gmail.com>
2016-12-26 22:31:53 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,7 +3,7 @@
 datetime <- paste(as.Date(newData$Date,"%d/%m/%Y"), newData$Time)
 newData$datetime <- as.POSIXct(datetime)
 
-png(filename="//Users/RuijianWang/Desktop/plot2.png", height=480, width=480,bg="white")
+png(filename="./Desktop/plot2.png", height=480, width=480,bg="white")
 with(newData, plot(datetime, Global_active_power, type = "l",
                    xlab='',ylab='Global Active Power (kilowatts)'))
 dev.off()
